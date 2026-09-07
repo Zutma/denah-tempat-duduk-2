dc2dbf57e32eaac641e1a4765b80e3f054d7c0ee
Zutma <rizviqarfajaraslamutama@gmail.com>
2026-09-07 22:34:39 +0700

    update pages

--- public/login.php
+++ public/login.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['name'];
-        header("Location: /dashboard.php");
+        header("Location: /dashboard");
         exit;
     }
 }
