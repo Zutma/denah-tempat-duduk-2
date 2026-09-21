94535a3cfab9435967d93d3d7cd4e03263e475ed
zoldyk168 <zoldyk168@gmail.com>
2026-09-21 08:52:30 +0700

    w

--- app/models/User.php
+++ app/models/User.php
@@ -2,7 +2,7 @@
 
 class User {
     public static function findByUsername($conn, $username) {
-        $stmt = $conn->prepare("SELECT * FROM users WHERE email = ? LIMIT 1");
+        $stmt = $conn->prepare("SELECT * FROM users WHERE name = ? LIMIT 1");
         $stmt->bind_param("s", $username);
         $stmt->execute();
         return $stmt->get_result()->fetch_assoc();
